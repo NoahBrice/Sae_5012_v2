4b9465f870c24843eb3e8f786f86bb05d74bb8c3
Noahm <no.nobrice77@gmail.com>
2024-01-12 12:59:55 +0100

    Easy Admin pour User

--- kixelapi/src/Controller/Admin/DashboardController.php
+++ kixelapi/src/Controller/Admin/DashboardController.php
@@ -2,34 +2,40 @@
 
 namespace App\Controller\Admin;
 
+use App\Controller\UserController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
-
+        
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
         // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
+        // return $this->redirect($adminUrlGenerator->setController(UserController::class)->generateUrl());
+        
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
         // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+            //     return $this->redirect('...');
+            // }
+            
+            // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+            // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+            //
+            // return $this->render('some/path/my-dashboard.html.twig');
+            return $this->render('/MenuCrud.html.twig');
+            
+            
+            return parent::index();
     }
 
     public function configureDashboard(): Dashboard
@@ -41,6 +47,7 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-list', User::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
---
+++ kixelapi/src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            // TextField::new('nom'),
+            // TextField::new('prenom'),
+            // TextField::new('password'),
+            // "id",
+            IdField::new('id')->onlyOnIndex(),
+            "nom",
+            "prenom",
+            "email",
+            "password",
+            ArrayField::new('roles'),
+            AssociationField::new('site')->setFormTypeOption('by_reference', false)->formatValue(function ($value, $entity) {
+                $sites = $entity->getSite();
+                $label = "";
+                foreach ($sites as $site) {
+                    $label = $label . $site->getNom() . "(" . $site->getId() . ")" . ", ";
+                }
+                return $label;
+            }),
+        ];
+    }
+   
+}
